dc55665f450f2365aaa8d9e153f50eeb019adc7f
Swarit <swarit.it@gmail.com>
2018-08-14 10:15:13 +0530

    spring boot admin server and client

---
+++ spring-admin/src/main/java/com/admin/spring/SpringAdminApplication.java
@@ -0,0 +1,15 @@
+package com.admin.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+}
---
+++ spring-client/src/main/java/com/client/spring/SpringClientApplication.java
@@ -0,0 +1,12 @@
+package com.client.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClientApplication.class, args);
+	}
+}
